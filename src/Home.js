5c1f1dbdb9be74738933fdc8eca6be2107564f36
Mateusz <youngbastardps4@gmail.com>
2021-02-15 15:11:57 +0100

    Initial commit

---
+++ src/Home.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import Banner from './Banner';
+import Card from './Card';
+import './Home.css';
+
+function Home() {
+    return (
+        <div className="home">
+            <Banner />
+            <div className="home__section">
+                <Card
+                    src="https://a0.muscache.com/im/pictures/a0316ecb-e49b-4b3a-b6b6-c2876b820e8c.jpg?im_w=720"
+                    title="Entire homes"
+                    description="Comfortable private places, with room for friends or family."
+                />
+                <Card
+                    src="https://a0.muscache.com/im/pictures/ff69ac49-64e7-4f4a-ae2b-ee01163d0790.jpg?im_w=720"
+                    title="Cabins and cottages"
+                    description="Spaces that are more than just a place to sleep."
+                />
+                <Card
+                    src="https://a0.muscache.com/im/pictures/ce6814ba-ed53-4d6e-b8f8-c0bbcf821011.jpg?im_w=720"
+                    title="Unique stays"
+                    description="Spaces that are more than just a place to sleep."
+                />
+                <Card
+                    src="https://a0.muscache.com/im/pictures/fbe849a4-841a-41b3-b770-419402a6316f.jpg?im_w=720"
+                    title="Pets  Welcome"
+                    description="Unique activities we ca do together, led by a world of hosts."
+                />
+            </div>
+            <div className="home__sectionDark">
+                <Card
+                    src="https://a0.muscache.com/im/pictures/2a16f833-464c-446c-8d74-33eb8c643975.jpg?im_w=720"
+                    title="Online Experiences"
+                    description="Unique activities we ca do together, led by a world of hosts."
+                />
+                <Card
+                    src="https://a0.muscache.com/im/pictures/426a8116-0b94-4407-ae87-924126c81d78.jpg?im_w=720"
+                    title="Unique stays"
+                    description="Spaces that are more than just a place to sleep."
+                />
+                <Card
+                    src="https://a0.muscache.com/im/pictures/a84e92bd-68e6-4ce2-9fdf-b2ce1a377f53.jpg?im_w=720"
+                    title="Entire homes"
+                    description="Comfortable private places, with room for friends or family."
+                />
+            </div>
+            <div className="home__section">
+                <Card
+                    src="https://a0.muscache.com/im/pictures/2a16f833-464c-446c-8d74-33eb8c643975.jpg?im_w=720"
+                    title="Host your home"
+                />
+                <Card
+                    src="https://a0.muscache.com/im/pictures/426a8116-0b94-4407-ae87-924126c81d78.jpg?im_w=720"
+                    title="Host an Online Experience"
+                />
+                <Card
+                    src="https://a0.muscache.com/im/pictures/a84e92bd-68e6-4ce2-9fdf-b2ce1a377f53.jpg?im_w=720"
+                    title="Host an Experience"
+                />
+            </div>
+        </div>
+    )
+}
+
+export default Home
